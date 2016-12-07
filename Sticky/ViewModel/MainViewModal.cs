bfdad870ef176ed6303aaaba2b157d04bf92975b
MaxMEllon <maxmellon1994@gmail.com>
2016-12-07 18:52:16 +0900

    :tada: first commit

---
+++ Sticky/ViewModel/MainViewModal.cs
@@ -0,0 +1,26 @@
+﻿using Sticky.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Sticky.ViewModel
+{
+    public class MainViewModel
+    {
+        public ICommand Drag { get; private set; }
+
+        private MainWindow view;
+        public MainViewModel(MainWindow view)
+        {
+            this.view = view;
+            Drag = new RelayCommand((_) =>
+            {
+                this.view.DragMove();
+            });
+        }
+    }
+
+}
